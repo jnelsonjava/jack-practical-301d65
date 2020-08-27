9da883c89bf8b275b451d28554101a3b08b3cccc
Jack Nelson <jnelson.java@gmail.com>
2020-08-27 14:41:01 -0700

    server started

---
+++ finalExamApp/server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// Packages
+
+const express = require('express');
+require('dotenv').config();
+
+// Global Variables
+
+const app = express();
+const PORT = process.env.PORT || 3003;
+
+// Express Configs
+
+// Routes
+
+// Route Handlers
+
+// Functions
+
+// Server Start
+
+app.listen(PORT, () => console.log(`server listening on PORT: ${PORT} ~`));
+
