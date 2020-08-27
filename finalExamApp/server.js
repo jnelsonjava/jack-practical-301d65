28e8b5896da63579434686f3c7d0ff4da4606d3d
Jack Nelson <jnelson.java@gmail.com>
2020-08-27 15:19:46 -0700

    rendering list of pokemon name results

--- finalExamApp/server.js
+++ finalExamApp/server.js
@@ -4,6 +4,7 @@
 
 const express = require('express');
 require('dotenv').config();
+const superagent = require('superagent');
 
 // Global Variables
 
@@ -12,9 +13,30 @@ const PORT = process.env.PORT || 3003;
 
 // Express Configs
 
+app.use(express.static('./public'));
+app.use(express.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
 // Routes
 
+app.get('/', renderPokemon);
+
 // Route Handlers
+
+function renderPokemon(req, res) {
+  const pokeUrl = `https://pokeapi.co/api/v2/pokemon?limit=50`;
+
+  superagent.get(pokeUrl)
+    .then( (pokeResult) => {
+      const sortedNames = pokeResult.body.results.map(poke => poke.name).sort();
+      res.render('pages/show', {names: sortedNames});
+
+    })
+  
+
+
+
+}
 
 // Functions
 
